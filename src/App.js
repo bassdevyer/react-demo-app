b9f9533abb3cf2330f8b9d63cd03eaac02a849e2
Miguel Torres <miguel@alturasoluciones.com>
2019-01-03 15:23:00 -0500

    React Router - Preventing Transitions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Prompt } from 'react-router-dom'
 
-class App extends Component {
-  render() {
+const App = () =>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Form</Link>
+        </li>
+        <li>
+          <Link to='/one'>One</Link>
+        </li>
+        <li>
+          <Link to='/two'>Two</Link>
+        </li>
+      </ul>
+      <Route path='/' exact component={Form}/>
+      <Route path='/one' render={() => <h3>One</h3>}/>
+      <Route path='/two' render={() => <h3>Two</h3>}/>
+    </div>
+  </Router>
+
+class Form extends Component {
+  state = {isBlocking: false}
+
+  render () {
+    let {isBlocking} = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          event.target.reset()
+          this.setState({
+            isBlocking: false
+          })
+        }}
+      >
+        <Prompt
+          when={isBlocking}
+          message={location =>
+            `Are you sure you want to go to ${location.pathname}`}
+        />
+        <p>
+          Blocking?{' '}
+          {isBlocking ? 'Yes, click a link or the back button' : 'Nope'}
+        </p>
+
+        <p>
+          <input
+            size='50'
+            placeholder='type something to block transitions'
+            onChange={event => {
+              this.setState({
+                isBlocking: event.target.value.length > 0
+              })
+            }}
+          />
+        </p>
+        
+        <p>
+          <button>Submit to stop blocking</button>
+        </p>
+
+      </form>
+    )
   }
+
 }
 
-export default App;
+export default App
