26f3d16528737b804bd57d74126ef91b677c7cf4
Miguel Torres <miguel@alturasoluciones.com>
2019-01-03 17:28:54 -0500

    React Router - Recursive Paths

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const PEEPS = [
+  {id: 0, name: 'Michelle', friends: [1, 2, 3]},
+  {id: 1, name: 'Sean', friends: [0, 3]},
+  {id: 2, name: 'Kim', friends: [0, 1, 3]},
+  {id: 3, name: 'David', friends: [1, 2]}
+]
+
+const find = (id) =>
+  PEEPS.find(p => p.id == id)
+
+const App = () =>
+  <Router>
+    <Person match={{params: {id: 0}, url: ''}}/>
+  </Router>
+
+const Person = ({match}) => {
+  let person = find(match.params.id)
+  return (
+    <div>
+      <h3>
+        {person.name}
+        's Friends
+      </h3>
+      <ul>
+        {person.friends.map(id => (
+            <li key={id}>
+              <Link to={`${match.url}/${id}`}>{find(id).name}</Link>
+            </li>
+          )
+        )}
+      </ul>
+      <Route path={`${match.url}/:id`} component={Person}/>
+    </div>
+  )
 }
 
-export default App;
+export default App
