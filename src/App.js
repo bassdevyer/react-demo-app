73ef4dde4dc784abffd97567b50abe09a437d492
Miguel Torres <miguel@alturasoluciones.com>
2019-01-04 09:06:26 -0500

    React Router - Ambiguous Matches

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/about'>About Us (static)</Link>
+        </li>
+        <li>
+          <Link to='/company'>Company (static)</Link>
+        </li>
+        <li>
+          <Link to='/kim'>Kim (dynamic)</Link>
+        </li>
+        <li>
+          <Link to='/chris'>Chris (dynamic)</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path='/about' component={About}/>
+        <Route path='/company' component={Company}/>
+        <Route path='/:user' component={User}/>
+      </Switch>
+    </div>
+  </Router>
 
-export default App;
+const About = () =>
+  <h2>About</h2>
+
+const Company = () =>
+  <h2>Company</h2>
+
+const User = ({match}) =>
+  <div>
+    <h2>User: {match.params.user}</h2>
+  </div>
+
+export default App
